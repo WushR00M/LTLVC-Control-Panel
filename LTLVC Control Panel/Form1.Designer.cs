b7a5d094c5fe1324392a710b29facf1784111326
WushR00M <moonthefox44@gmail.com>
2025-09-13 13:03:58 -0600

    Stopwatch + time counter + "End Competition" button which saves the times, places, and more to a document in your Documents folder!

--- LTLVC Control Panel/Form1.Designer.cs
+++ LTLVC Control Panel/Form1.Designer.cs
@@ -29,12 +29,261 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startTimerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopTimerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.manageContestantsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.endCompetitionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startCompetitionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripSeparator();
+            this.startStopwatchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.minutesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.minutesToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.minutesToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.minutesToolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.minutesToolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.minuteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.secondsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startStopwatchToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.startTimerToolStripMenuItem,
+            this.stopTimerToolStripMenuItem,
+            this.toolStripMenuItem3,
+            this.startCompetitionToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.manageContestantsToolStripMenuItem,
+            this.endCompetitionToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.fileToolStripMenuItem.Text = "Tools";
+            // 
+            // startTimerToolStripMenuItem
+            // 
+            this.startTimerToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.minutesToolStripMenuItem,
+            this.minutesToolStripMenuItem1,
+            this.minutesToolStripMenuItem2,
+            this.minutesToolStripMenuItem3,
+            this.minutesToolStripMenuItem4,
+            this.minuteToolStripMenuItem,
+            this.secondsToolStripMenuItem});
+            this.startTimerToolStripMenuItem.Name = "startTimerToolStripMenuItem";
+            this.startTimerToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.startTimerToolStripMenuItem.Text = "Start Timer";
+            // 
+            // stopTimerToolStripMenuItem
+            // 
+            this.stopTimerToolStripMenuItem.Name = "stopTimerToolStripMenuItem";
+            this.stopTimerToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.stopTimerToolStripMenuItem.Text = "Stop Ongoing Timers";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(183, 6);
+            // 
+            // manageContestantsToolStripMenuItem
+            // 
+            this.manageContestantsToolStripMenuItem.Name = "manageContestantsToolStripMenuItem";
+            this.manageContestantsToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.manageContestantsToolStripMenuItem.Text = "Manage Contestants";
+            // 
+            // endCompetitionToolStripMenuItem
+            // 
+            this.endCompetitionToolStripMenuItem.BackColor = System.Drawing.Color.DarkRed;
+            this.endCompetitionToolStripMenuItem.ForeColor = System.Drawing.SystemColors.Control;
+            this.endCompetitionToolStripMenuItem.Name = "endCompetitionToolStripMenuItem";
+            this.endCompetitionToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.endCompetitionToolStripMenuItem.Text = "End Competition";
+            this.endCompetitionToolStripMenuItem.Click += new System.EventHandler(this.endCompetitionToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // startCompetitionToolStripMenuItem
+            // 
+            this.startCompetitionToolStripMenuItem.Name = "startCompetitionToolStripMenuItem";
+            this.startCompetitionToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.startCompetitionToolStripMenuItem.Text = "Start Competition";
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(183, 6);
+            // 
+            // startStopwatchToolStripMenuItem
+            // 
+            this.startStopwatchToolStripMenuItem.BackColor = System.Drawing.Color.SeaGreen;
+            this.startStopwatchToolStripMenuItem.ForeColor = System.Drawing.SystemColors.Control;
+            this.startStopwatchToolStripMenuItem.Name = "startStopwatchToolStripMenuItem";
+            this.startStopwatchToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.startStopwatchToolStripMenuItem.Text = "Start Competition";
+            this.startStopwatchToolStripMenuItem.Click += new System.EventHandler(this.startStopwatchToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(183, 6);
+            // 
+            // minutesToolStripMenuItem
+            // 
+            this.minutesToolStripMenuItem.Name = "minutesToolStripMenuItem";
+            this.minutesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.minutesToolStripMenuItem.Text = "15 Minutes";
+            // 
+            // minutesToolStripMenuItem1
+            // 
+            this.minutesToolStripMenuItem1.Name = "minutesToolStripMenuItem1";
+            this.minutesToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.minutesToolStripMenuItem1.Text = "10 Minutes";
+            // 
+            // minutesToolStripMenuItem2
+            // 
+            this.minutesToolStripMenuItem2.Name = "minutesToolStripMenuItem2";
+            this.minutesToolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
+            this.minutesToolStripMenuItem2.Text = "5 Minutes";
+            // 
+            // minutesToolStripMenuItem3
+            // 
+            this.minutesToolStripMenuItem3.Name = "minutesToolStripMenuItem3";
+            this.minutesToolStripMenuItem3.Size = new System.Drawing.Size(180, 22);
+            this.minutesToolStripMenuItem3.Text = "3 Minutes";
+            // 
+            // minutesToolStripMenuItem4
+            // 
+            this.minutesToolStripMenuItem4.Name = "minutesToolStripMenuItem4";
+            this.minutesToolStripMenuItem4.Size = new System.Drawing.Size(180, 22);
+            this.minutesToolStripMenuItem4.Text = "2 Minutes";
+            // 
+            // minuteToolStripMenuItem
+            // 
+            this.minuteToolStripMenuItem.Name = "minuteToolStripMenuItem";
+            this.minuteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.minuteToolStripMenuItem.Text = "1 Minute";
+            // 
+            // secondsToolStripMenuItem
+            // 
+            this.secondsToolStripMenuItem.Name = "secondsToolStripMenuItem";
+            this.secondsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.secondsToolStripMenuItem.Text = "30 Seconds";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(109, 17);
+            this.toolStripStatusLabel1.Text = "Elapsed Time... 0:00";
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(103, 17);
+            this.toolStripStatusLabel2.Text = "Initializing Clock...";
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximumSize = new System.Drawing.Size(816, 489);
+            this.Name = "Form1";
+            this.Text = "LTLVC: Control Panel";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startStopwatchToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem startTimerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem minutesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem minutesToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem minutesToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem minutesToolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem minutesToolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem minuteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem secondsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopTimerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem startCompetitionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem manageContestantsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem endCompetitionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.Timer timer3;
     }
 }
 
--- LTLVC Control Panel/Form1.cs
+++ LTLVC Control Panel/Form1.cs
@@ -3,18 +3,143 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace LTLVC_Control_Panel
 {
     public partial class Form1 : Form
     {
+        Int32 sec = 0;
+        Int32 min = 0;
+        Int32 hr = 0;
+        Int32 day = 0;
+
+        String below10 = ":";
+        String below1 = ":";
+        String below1_2 = ":";
+
+        String startingtime;
         public Form1()
         {
             InitializeComponent();
+            timer3.Start(); // timer to get current date and time
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            About about = new About();
+            about.Show();
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            DateTime currentTime = DateTime.Now;
+            toolStripStatusLabel2.Text = Convert.ToString(currentTime);
+        }
+
+        private void startStopwatchToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Start(); // timer to get current elasped time
+
+            DateTime currentTime = DateTime.Now;
+            startingtime = Convert.ToString(currentTime);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // time calculation
+            sec += 1;
+            if (sec == 60)
+            {
+                sec = 0;
+                min += 1;
+
+                if (min == 60)
+                {
+                    min = 0;
+                    hr += 1;
+
+                    if (hr == 24)
+                    {
+                        day += 1;
+                        hr = 0;
+                    }
+                }
+            }
+            
+            // proper time formatting (part 1)
+            if (sec < 10) below10 = ":0";
+            else below10 = ":";
+
+            if (min < 10) below1 = ":0";
+            else below1 = ":";
+
+            if (hr < 10) below1_2 = ":0";
+            else below1_2 = ":";
+
+            // proper time formatting (part 2 + sending to toolstrip)
+            if (hr == 0 && day == 0)
+                toolStripStatusLabel1.Text = "Elapsed time... " + Convert.ToString(min) + below10 + Convert.ToString(sec);
+            else if (hr >= 1 && day == 0)
+                toolStripStatusLabel1.Text = "Elapsed time... " + Convert.ToString(hr) + below1 + Convert.ToString(min) + below10 + Convert.ToString(sec);
+            else
+                toolStripStatusLabel1.Text = "Elapsed time... " + Convert.ToString(day) + below1_2 + Convert.ToString(hr) + below1 + Convert.ToString(min) + below10 + Convert.ToString(sec);
+        }
+        private void endCompetitionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            DateTime currentTime = DateTime.Now;
+            string endingTime = Convert.ToString(currentTime);
+
+            DialogResult endresult = MessageBox.Show(
+
+                "Competition complete! Would you like to save the statistics into a text file for later viewing?",
+                "Competition complete, elapsed time: " + Convert.ToString(day) + below1_2 + Convert.ToString(hr) + below1 + Convert.ToString(min) + below10 + Convert.ToString(sec),
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question,
+                MessageBoxDefaultButton.Button1
+                
+                );
+
+            if (endresult == DialogResult.Yes)
+            {
+                try
+                {
+                    string[] txtline = { "Elapsed Time: " + Convert.ToString(day) + below1_2 + Convert.ToString(hr) + below1 + Convert.ToString(min) + below10 + Convert.ToString(sec), "Starting Time: " + startingtime, "Ending Time: " + endingTime };
+                    string docPath =
+                        Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                    using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "LTLVC_Results.txt")))
+                    {
+                        foreach (string line in txtline)
+                            outputFile.WriteLine(line);
+                    }
+
+                    MessageBox.Show(
+                        "Your statistics were saved to your Documents folder.",
+                        "Save complete!",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information,
+                        MessageBoxDefaultButton.Button1
+                    );
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(
+                        "An error occured whilst saving. Your statistics have not been saved.",
+                        "Save Error",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error,
+                        MessageBoxDefaultButton.Button1
+                    );
+                }
+            }
+            else { }
         }
     }
 }
